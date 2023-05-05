b2135d9ceecacba224e9e0106509f72c211b0101
mstoolmiller <51488173+mstoolmiller@users.noreply.github.com>
2023-05-05 12:23:52 -0400

    Trying to solve the problem of building the package

--- R/alt_axes1.r
+++ R/alt_axes1.r
@@ -1,5 +1,4 @@
-#This is important function
-## Yes, indeed.
+#' @param i input value
 alt_axes <- function(i) {
 # i is a subplot counter, from 1 to the total number of subplots = prod(par("mfrow"))
 	# for horizontal axis
