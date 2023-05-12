3d7f23338e935e399071f8c75b9c6518dc05091d
MikeS <mike.stoolmiller@yahoo.com>
2023-05-12 12:48:26 -0400

    still testing github connection

--- R/alt_axes1.r
+++ R/alt_axes1.r
@@ -3,7 +3,7 @@
 #' These lines provide a verbose description.
 #' This could be multiple lines
 #'
-#' @param i An integer value. a subplot counter, from 1 to the total number of subplots = prod(par("mfrow"))
+#' @param i An integer value. A subplot counter, from 1 to the total number of subplots = prod(par("mfrow"))
 #'
 #' @examples
 #' # Some code
